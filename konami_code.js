01dec99edfcd796ebd9857ee4deb90e2f466c80f
Betsy Sallee <ensallee@gmail.com>
2018-01-29 03:12:03 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,26 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let body=document.querySelector('.body');
+  
+  let index=0;
+  
+  function onKeyDownHandler(e){
+    const key=parseInt(e.detal || e.which);
+    
+    if(key===code[index]){
+      index++;
+      
+    if(index===code.length){
+      alert("You did it!");
+      
+      index=0;
+    }
+    } else{
+      index=0;
+  }
+}
+
+body.addEventListener('keydown', onKeyDownHandler(e));
 
 }
