c09a9e3cedf8014a68b07c16f19f44c708195a03
Betsy Sallee <ensallee@gmail.com>
2018-01-29 03:35:33 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,21 +3,19 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 function init() {
   let body=document.querySelector('body');
   let index=0;
- 
-  function onKeyDownHandler(e){
-    const key=parseInt(e.detail || e.which);
+ body.addEventListener('keydown', function onKeyDownHandler(e){
+ const key=parseInt(e.detail || e.which);
     
-    if(key===code[index]){
-      index++;
+        if(key===code[index]){
+          index++;
       
-    if(index===code.length){
-      alert("You did it!");
+            if(index===code.length){
+              alert("You did it!");
       
-      index=0;
-    }
-    } else{
-      index=0;
-  }
-}
- body.addEventListener('keydown', onKeyDownHandler(e));
+              index=0;
+          }
+        } else{
+          index=0;
+     }
+  });
 }
