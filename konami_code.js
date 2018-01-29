9cb17fb4103f751cef337524a5c6801788e358a8
Betsy Sallee <ensallee@gmail.com>
2018-01-29 03:23:03 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,10 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  let body=document.querySelector('.body');
+  let body=document.querySelector('body');
   let index=0;
  
-  function onKeyHandler(e){
+  function onKeyDownHandler(e){
     const key=parseInt(e.detail || e.which);
     
     if(key===code[index]){
